c0bee00ebe3ff9762e2a952e89de7dd8864d4d62
lucas_andretti <andrettilucas9@gmail.com>
2024-05-23 20:28:23 -0300

    Lista 1.0

---
+++ main.py
@@ -0,0 +1,20 @@
+# lista
+nomes = ['Lucas','Carvalho','Araujo','Andretti','Isabela']
+
+#exibe a lista de nomes
+for i in range(len(nomes)):
+    print(f'{i + 1}º nome da lista: {nomes[i]}.')
+
+# ordenando a lista
+nomes.sort(reverse=True)
+
+
+# exibe a lista na tela 
+for nome in nomes:
+    print(nome) 
+
+
+
+          
+
+
